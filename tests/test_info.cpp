c0b342a48392a6243c08579279a9e2f429264a90
Tenaryo <tylertan@sjtu.edu.cn>
2026-04-01 03:42:31 +0000

    test: add master_replid and master_repl_offset tests

--- tests/test_info.cpp
+++ tests/test_info.cpp
@@ -47,6 +47,43 @@ void test_info_without_args_returns_replication_section() {
         << "\u2713 Test 2 passed: INFO without args returns replication section with role:master\n";
 }
 
+auto extract_info_content = [](const std::string& response) -> std::string {
+    assert(response.starts_with("$"));
+    auto crlf = response.find("\r\n");
+    assert(crlf != std::string::npos);
+    int len = std::stoi(response.substr(1, crlf - 1));
+    return response.substr(crlf + 2, len);
+};
+
+void test_info_master_replid() {
+    Store store;
+    ServerConfig config;
+    CommandHandler handler(store, config);
+
+    std::string input = "*2\r\n$4\r\nINFO\r\n$11\r\nreplication\r\n";
+    auto response = handler.process(input);
+    auto content = extract_info_content(response);
+
+    assert(content.find("master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb") !=
+           std::string::npos);
+
+    std::cout << "\u2713 Test 4 passed: INFO returns correct master_replid\n";
+}
+
+void test_info_master_repl_offset() {
+    Store store;
+    ServerConfig config;
+    CommandHandler handler(store, config);
+
+    std::string input = "*2\r\n$4\r\nINFO\r\n$11\r\nreplication\r\n";
+    auto response = handler.process(input);
+    auto content = extract_info_content(response);
+
+    assert(content.find("master_repl_offset:0") != std::string::npos);
+
+    std::cout << "\u2713 Test 5 passed: INFO returns correct master_repl_offset\n";
+}
+
 void test_info_replication_returns_role_slave_when_configured() {
     Store store;
     ServerConfig config;
@@ -74,6 +111,8 @@ int main() {
 
     test_info_replication_returns_role_master();
     test_info_without_args_returns_replication_section();
+    test_info_master_replid();
+    test_info_master_repl_offset();
     test_info_replication_returns_role_slave_when_configured();
 
     std::cout << "\n\u2713 All tests passed!\n";
