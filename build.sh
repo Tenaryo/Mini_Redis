bb4c1be8030527cd557b56cd3bd8780ff1fa9171
Tenaryo <tylertan@sjtu.edu.cn>
2026-04-12 09:38:19 +0000

    feat: use PCH and ninja for compilation

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 BUILD_DIR="build"
 BUILD_TYPE="${1:-Debug}"
 
-cmake -B "$BUILD_DIR" -S . \
+cmake -B "$BUILD_DIR" -S . -G Ninja \
     -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
     -DCMAKE_CXX_COMPILER=g++ \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
