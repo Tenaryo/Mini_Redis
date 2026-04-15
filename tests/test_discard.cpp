856fa93ef13871a2ee90d8a2646b5cb0df3bbfb9
Tenaryo <tylertan@sjtu.edu.cn>
2026-04-15 02:15:19 +0000

    test: add DISCARD clears watch state test

--- tests/test_discard.cpp
+++ tests/test_discard.cpp
@@ -48,12 +48,44 @@ void test_discard_then_exec_returns_error() {
     std::cout << "\u2713 Test 3 passed: EXEC after DISCARD returns error\n";
 }
 
+void test_discard_clears_watch_state_allows_subsequent_transaction() {
+    Store store;
+    CommandHandler handler(store);
+
+    handler.process_with_fd(1, "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n100\r\n", nullptr);
+    handler.process_with_fd(1, "*3\r\n$3\r\nSET\r\n$3\r\nbar\r\n$3\r\n200\r\n", nullptr);
+
+    handler.process_with_fd(1, "*3\r\n$5\r\nWATCH\r\n$3\r\nfoo\r\n$3\r\nbar\r\n", nullptr);
+
+    handler.process_with_fd(1, "*1\r\n$5\r\nMULTI\r\n", nullptr);
+    handler.process_with_fd(1, "*3\r\n$3\r\nSET\r\n$3\r\nbar\r\n$3\r\n300\r\n", nullptr);
+
+    handler.process_with_fd(2, "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n400\r\n", nullptr);
+
+    auto discard_result = handler.process_with_fd(1, "*1\r\n$7\r\nDISCARD\r\n", nullptr);
+    assert(discard_result.response == "+OK\r\n");
+
+    handler.process_with_fd(1, "*1\r\n$5\r\nMULTI\r\n", nullptr);
+    handler.process_with_fd(1, "*3\r\n$3\r\nSET\r\n$3\r\nbar\r\n$3\r\n300\r\n", nullptr);
+
+    auto exec_result = handler.process_with_fd(1, "*1\r\n$4\r\nEXEC\r\n", nullptr);
+    assert(exec_result.response == "*1\r\n+OK\r\n");
+
+    auto bar_val = store.get("bar");
+    assert(bar_val.has_value());
+    assert(bar_val.value() == "300");
+
+    std::cout
+        << "\u2713 Test 4 passed: DISCARD clears watch state, subsequent transaction succeeds\n";
+}
+
 int main() {
     std::cout << "Running DISCARD command tests...\n\n";
 
     test_discard_in_multi_returns_ok();
     test_discard_without_multi_returns_error();
     test_discard_then_exec_returns_error();
+    test_discard_clears_watch_state_allows_subsequent_transaction();
 
     std::cout << "\n\u2713 All tests passed!\n";
     return 0;
